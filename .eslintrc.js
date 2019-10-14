55b82af51ee63300505208192229a43fad8dd9f6
vologa <bloshchitsyn@gmail.com>
2019-10-14 10:25:10 +0300

    add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es6': false,
+        'node': true,
+        'mocha': true,
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 5
+    },
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'max-len': ["error", { "code": 120 }]
+    }
+};
